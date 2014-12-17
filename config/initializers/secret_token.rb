8bc81809321eb2cd9b3c32d1f2f028a07b6755c1
EC2 Default User <ec2-user@ip-172-31-11-17.ap-southeast-1.compute.internal>
2014-12-17 03:54:39 +0000

    initial commit? :

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Brandpage::Application.config.secret_key_base = '3d0a730ddb1e924970b5f94ad02261a862cf71b5b9d0df50d1d56318c0b3cecaeefbc2f1ef09c4568cbcecd467cb8cdc5277d9032aa8d9c2ddae64a8bb4905dc'
